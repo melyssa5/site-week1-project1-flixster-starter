527cffe2401c4def1ba0cfcbeb6695b9a9efb176
Melyssa Cristino <mcristino@mcristi-ltm6kem.internal.salesforce.com>
2023-06-08 01:13:55 -0700

    little more layout

--- script.js
+++ script.js
@@ -72,6 +72,69 @@ let fakeMoviesAPI = {
             "vote_average": 7.1,
             "vote_count": 854
         },
+        {
+            "adult": false,
+            "backdrop_path": "/2I5eBh98Q4aPq8WdQrHdTC8ARhY.jpg",
+            "genre_ids": [
+                28,
+                12,
+                16,
+                878
+            ],
+            "id": 569094,
+            "original_language": "en",
+            "original_title": "Spider-Man: Across the Spider-Verse",
+            "overview": "After reuniting with Gwen Stacy, Brooklyn’s full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters the Spider Society, a team of Spider-People charged with protecting the Multiverse’s very existence. But when the heroes clash on how to handle a new threat, Miles finds himself pitted against the other Spiders and must set out on his own to save those he loves most.",
+            "popularity": 2921.844,
+            "poster_path": "/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg",
+            "release_date": "2023-05-31",
+            "title": "Spider-Man: Across the Spider-Verse",
+            "video": false,
+            "vote_average": 8.8,
+            "vote_count": 739
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/2I5eBh98Q4aPq8WdQrHdTC8ARhY.jpg",
+            "genre_ids": [
+                28,
+                12,
+                16,
+                878
+            ],
+            "id": 569094,
+            "original_language": "en",
+            "original_title": "Spider-Man: Across the Spider-Verse",
+            "overview": "After reuniting with Gwen Stacy, Brooklyn’s full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters the Spider Society, a team of Spider-People charged with protecting the Multiverse’s very existence. But when the heroes clash on how to handle a new threat, Miles finds himself pitted against the other Spiders and must set out on his own to save those he loves most.",
+            "popularity": 2921.844,
+            "poster_path": "/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg",
+            "release_date": "2023-05-31",
+            "title": "Spider-Man: Across the Spider-Verse",
+            "video": false,
+            "vote_average": 8.8,
+            "vote_count": 739
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/2I5eBh98Q4aPq8WdQrHdTC8ARhY.jpg",
+            "genre_ids": [
+                28,
+                12,
+                16,
+                878
+            ],
+            "id": 569094,
+            "original_language": "en",
+            "original_title": "Spider-Man: Across the Spider-Verse",
+            "overview": "After reuniting with Gwen Stacy, Brooklyn’s full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters the Spider Society, a team of Spider-People charged with protecting the Multiverse’s very existence. But when the heroes clash on how to handle a new threat, Miles finds himself pitted against the other Spiders and must set out on his own to save those he loves most.",
+            "popularity": 2921.844,
+            "poster_path": "/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg",
+            "release_date": "2023-05-31",
+            "title": "Spider-Man: Across the Spider-Verse",
+            "video": false,
+            "vote_average": 8.8,
+            "vote_count": 739
+        },
     ],
     "total_pages": 98,
     "total_results": 1951
@@ -131,11 +194,10 @@ function generateMovieCard(movieObject){
     movie.appendChild(image);
     movie.appendChild(averageContainer);
     movie.appendChild(title);
-    //movieContainer.appendChild(movie)
+
     return movie;
 }
 
-generateMovieCard(fakeMoviesAPI.results[0]);
 displayMovies(fakeMoviesAPI.results);
 
 
